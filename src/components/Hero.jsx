0c95849a964880a6e278948d51bb41873c7c8a0e
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-18 17:23:51 +0100

    Update Hero with introduction text & header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,21 @@ const Hero = () => {
         src="https://static.vecteezy.com/system/resources/previews/012/865/509/large_2x/maldives-aerial-island-landscape-tropical-beach-coast-from-drone-exotic-nature-palm-trees-over-white-sand-close-to-coral-reef-blue-sea-lagoon-summer-and-travel-vacation-concept-beautiful-nature-photo.jpg"
         alt="/"
       />
+      <div>
+        <div>
+          <h1>Find Your Special Trip</h1>
+          <h2>With Traveler's Oasis</h2>
+          <p>
+            Looking for a unique and unforgettable travel experience? Look no
+            further than Traveler's Oasis! Our team of travel experts is
+            dedicated to helping you find your perfect trip, whether you're
+            seeking adventure, relaxation, or something in between. With
+            Traveler's Oasis, you'll discover new destinations, create lasting
+            memories, and have the time of your life. Start planning your dream
+            trip today!
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
