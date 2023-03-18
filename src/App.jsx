7e47f9a3bbb7d57bb2434c68b06e0cbab115351e
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-18 17:22:21 +0100

    Add Hero component with image

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import TopNav from "./components/TopNav"
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 function App() {
 return (
   <div>
     <TopNav />
     <Navbar />
+    <Hero />
   </div>
 )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/012/865/509/large_2x/maldives-aerial-island-landscape-tropical-beach-coast-from-drone-exotic-nature-palm-trees-over-white-sand-close-to-coral-reef-blue-sea-lagoon-summer-and-travel-vacation-concept-beautiful-nature-photo.jpg"
+        alt="/"
+      />
+    </div>
+  );
+};
+
+export default Hero;
