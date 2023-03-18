d14d88ee4a67f1a3eee83e63b0336f83a716878f
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-18 16:35:16 +0100

    Add h1 styles to TopNav

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -7,14 +7,14 @@ const TopNav = () => {
     <div className="flex justify-between items-center px-4 py-2">
       <div className="flex items-center">
         <BsChatSquareDots size={30} className='text-[var(--primary-dark)] mr-2'/>
-      <h1>TRAVELER'S OASIS</h1>
+      <h1 className="text-xl font-bold text-gray-700">TRAVELER'S OASIS</h1>
       </div>
       <div className="flex">
-        <div className="hidden md:flex item-center px-6">
+        <div className="hidden md:flex items-center px-6">
             <AiOutlineClockCircle />
             <p className="text-sm text-gray-700">9AM - 5AM</p>
         </div>
-        <div className="hidden md:flex item-center px-6">
+        <div className="hidden md:flex items-center px-6">
             <AiFillPhone  size={20} className='mr-2 text-[var(--primary-dark)]'/>
             <p className="text-sm text-gray-700">1-777-718-1234</p>
         </div>
