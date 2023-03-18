86986bbe110cd56ac2b16ea8eef72d0e4d1b8d3f
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-18 13:01:12 +0100

    Add style and size to BsChatSquareDots icon

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -6,7 +6,7 @@ const TopNav = () => {
   return (
     <div className="flex justify-between items-center px-4 py-2">
       <div className="flex items-center">
-        <BsChatSquareDots />
+        <BsChatSquareDots size={30} className='text-[var(--primary-dark)] mr-2'/>
       <h1>TRAVELER'S OASIS</h1>
       </div>
       <div>
