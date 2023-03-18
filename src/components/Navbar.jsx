ac4f84b7620d02117bcc66e85c7fde599b516d2f
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-18 17:14:26 +0100

    Modify Navbar to use state for mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,12 @@ import {
 import { BsChatSquareDots } from "react-icons/bs";
 
 const Navbar = () => {
+ const [nav, setNav] = useState(false)
+
+ const navHandler = () => {
+    setNav(!nav)
+ }
+
   return (
     <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80">
       <ul className="hidden sm:flex px-4">
@@ -32,22 +38,22 @@ const Navbar = () => {
         <FaTwitter className="mx-4" />
       </div>
       {/* Hamburger*/}
-      <div className="sm:hidden z-10">
+      <div onClick={navHandler} className="sm:hidden z-10">
         <FaBars size={20} className='mr-4 cursor-pointer'/>
       </div>
       {/*Mobile Menu*/}
-      <div className={'overflow-y-hidden md:hidden w-full ease-in duration-300 absolute text-gray-300 left-0 top-0 h-screen bg-black/90 px-4 py-7 flex flex-col'}>
-        <ul>
-            <li>
+      <div className={nav ? 'overflow-y-hidden md:hidden w-full ease-in duration-300 absolute text-gray-300 left-0 top-0 h-screen bg-black/90 px-4 py-7 flex flex-col': 'absolute top-0 h-screen left-[-100%] ease-in duration-500'}>
+        <ul className="h-full w-full text-center pt-12">
+            <li className="text-2xl py-8 ">
               <a href="/">Home</a>
             </li>
-            <li>
+            <li className="text-2xl py-8 ">
               <a href="#gallery">Gallery</a>
             </li>
-            <li>
+            <li className="text-2xl py-8 ">
               <a href="#deals">Deals</a>
             </li>
-            <li>
+            <li className="text-2xl py-8 ">
               <a href="#contact">Contact</a>
             </li>
         </ul>
