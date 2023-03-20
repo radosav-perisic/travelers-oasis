9c51fbf46b8da8982ea784fbaefdf9c6109dfcd3
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-20 19:51:02 +0100

    Update Activities section styling

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -6,11 +6,11 @@ import resort3 from '../assets/resort3.jpg'
 
 
 const Activities = () => {
-  return (
-    <div>
+  return (  
+    <div className="max-w-[1140px] m-auto w-full md:flex mt-[-75px]">
       <div>
         <h3>Resorts</h3>
-        <img src={resort1} alt="/" />
+        <img src={resort1} alt="/"/>
       </div>
       <div>
         <h3>Resorts</h3>
