8c9d7f9008e64bc2dea9a63dc336679c2a0762f5
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-21 13:27:09 +0100

    Improved image display in contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,7 +5,7 @@ const ContactForm = () => {
     <div id='contact' className='max-w-[1140px] m-auto w-full p-4 py-16'>
         <h2 className='text-center text-gray-600'>Contact Us Anytime</h2>
         <p className='text-center text-gray-600 py-2'>We're just a message away!</p>
-        <div>
+        <div className='grid md:grid-cols-2'>
             <img src='https://images.pexels.com/photos/3580532/pexels-photo-3580532.jpeg' alt='/' />
         </div>
         <form>
