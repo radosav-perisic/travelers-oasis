44d95809228912f70473205dfe8cdd23b11ddbfc
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-21 15:22:53 +0100

    Refactor ContactForm with improved structure

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -13,18 +13,31 @@ const ContactForm = () => {
           alt="/"
           className="w-full md:h-full object-cover p-2 max-h-[500px] h-[200px]"
         />
+
+        <form>
+          <div className="grid grid-cols-2">
+            <input className="border m-2 p-2" type="text" placeholder="First" />
+            <input className="border m-2 p-2" type="text" placeholder="Last" />
+            <input
+              className="border m-2 p-2"
+              type="email"
+              placeholder="Email"
+            />
+            <input className="border m-2 p-2" type="tel" placeholder="Phone" />
+            <input
+              className="border col-span-2 p-2 m-2"
+              type="text"
+              placeholder="Address"
+            />
+            <textarea
+              className="border col-span-2 m-2 p-2"
+              rows="10"
+              cols="30"
+            ></textarea>
+            <button className="col-span-2 m-2">Submit</button>
+          </div>
+        </form>
       </div>
-      <form> 
-        <div className="grid grid-cols-2">
-          <input className="border m-2 p-2" type="text" placeholder="First" />
-          <input className="border m-2 p-2" type="text" placeholder="Last" />
-          <input className="border m-2 p-2" type="email" placeholder="Email" />
-          <input className="border m-2 p-2" type="tel" placeholder="Phone" />
-          <input className="border col-span-2 p-2 m-2" type="text" placeholder="Address" />
-          <textarea className="border col-span-2 m-2 p-2" rows="10" cols="30"></textarea>
-          <button className="col-span-2 m-2">Submit</button>
-        </div>
-      </form>
     </div>
   );
 };
