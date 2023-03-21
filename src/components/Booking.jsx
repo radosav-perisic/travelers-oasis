5b5af756318d6f5d152fd264d0bd97a1f000afee
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-21 10:48:27 +0100

    Create Booking section

---
+++ src/components/Booking.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Booking = () => {
+  return (
+    <div id='deals'>
+        <form>
+            <div>
+                <label>Destination</label>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Booking
