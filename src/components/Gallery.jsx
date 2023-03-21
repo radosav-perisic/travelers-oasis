265c0cb80116963178f908cdc5273f3e1a17a954
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-21 12:24:57 +0100

    Add styling to Gallery title

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Gallery = () => {
   return (
     <div id='gallery' className='max-w-[1140] m-auto w-full px-4 py-16'>
-        <h2>Gallery </h2>
+        <h2 className='text-center text-gray-600 p-4'>Gallery </h2>
         <div>
             <div>
                 <img src='https://images.unsplash.com/photo-1613553497126-a44624272024?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='/'/>
