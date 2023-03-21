2a653fcbb3175cb34100813b984feb41b97922bf
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-21 13:13:48 +0100

    Add image to ContactForm

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,7 +4,10 @@ const ContactForm = () => {
   return (
     <div id='contact'>
         <h2>Contact Us Anytime</h2>
-        <p>We're just a message away</p>
+        <p>We're just a message away!</p>
+        <div>
+            <img src='https://images.pexels.com/photos/3580532/pexels-photo-3580532.jpeg' alt='/' />
+        </div>
     </div>
   )
 }
