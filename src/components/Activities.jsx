380de83728df89b506b314087bcac5653b934e7e
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-21 19:52:14 +0100

    Swap order of Activities for improved appearance

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -13,13 +13,14 @@ const Activities = () => {
         <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={resort} alt="/"/>
       </div>
       <div className="relative p-4 ">
+        <h3 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Excursions</h3>
+        <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={excursion} alt="/" />
+      </div>
+      <div className="relative p-4 ">
         <h3 className="z-10 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Cruises</h3>
         <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={cruise} alt="/" />
       </div>
-      <div className="relative p-4 ">
-        <h3 className="z-10 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Excursions</h3>
-        <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={excursion} alt="/" />
-      </div>
+      
     </div>
   );
 };
