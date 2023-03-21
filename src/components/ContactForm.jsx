86e0204b33652885a723b178f337ea6030eb5904
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-21 13:39:02 +0100

    Adjust spacing between form elements

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -14,14 +14,14 @@ const ContactForm = () => {
           className="w-full md:h-full object-cover p-2 max-h-[500px] h-[200px]"
         />
       </div>
-      <form>
+      <form> 
         <div className="grid grid-cols-2">
-          <input type="text" placeholder="First" />
-          <input type="text" placeholder="Last" />
-          <input type="email" placeholder="Email" />
-          <input type="tel" placeholder="Phone" />
-          <input type="text" placeholder="Address" />
-          <textarea rows="10" cols="30"></textarea>
+          <input className="border m-2 p-2" type="text" placeholder="First" />
+          <input className="border m-2 p-2" type="text" placeholder="Last" />
+          <input className="border m-2 p-2" type="email" placeholder="Email" />
+          <input className="border m-2 p-2" type="tel" placeholder="Phone" />
+          <input className="border col-span-2 p-2 m-2" type="text" placeholder="Address" />
+          <textarea className="border col-span-2 m-2 p-2" rows="10" cols="30"></textarea>
           <button>Submit</button>
         </div>
       </form>
