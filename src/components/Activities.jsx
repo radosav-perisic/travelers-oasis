51eae10663899e46df2ef503028f226cfc572376
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-21 15:55:16 +0100

    Update activity images in Activities

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import resort1 from '../assets/resort1.jpg'
-import resort2 from '../assets/resort2.jpg'
-import resort3 from '../assets/resort3.jpg'
+import resort from '../assets/resort.jpg'
+import cruise from '../assets/cruise.jpg'
+import excursion from '../assets/excursion.jpg'
 
 
 
@@ -10,15 +10,15 @@ const Activities = () => {
     <div className="max-w-[1140px] m-auto w-full md:flex mt-[-75px]">
       <div className="relative p-4 ">
         <h3 className="z-10 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Resorts</h3>
-        <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={resort1} alt="/"/>
+        <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={resort} alt="/"/>
       </div>
       <div className="relative p-4 ">
         <h3 className="z-10 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Cruises</h3>
-        <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={resort2} alt="/" />
+        <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={cruise} alt="/" />
       </div>
       <div className="relative p-4 ">
         <h3 className="z-10 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Excursions</h3>
-        <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={resort3} alt="/" />
+        <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={excursion} alt="/" />
       </div>
     </div>
   );
