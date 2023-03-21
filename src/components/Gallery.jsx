1d21f3b7cbe96b6a8a8dffafb16df7aabb9e1f6a
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-21 12:28:57 +0100

    Update gallery to use grid layout

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,7 +4,7 @@ const Gallery = () => {
   return (
     <div id='gallery' className='max-w-[1140] m-auto w-full px-4 py-16'>
         <h2 className='text-center text-gray-600 p-4'>Gallery </h2>
-        <div>
+        <div className='grid sm:grid-cols-5 gap-4'>
             <div>
                 <img src='https://images.unsplash.com/photo-1613553497126-a44624272024?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='/'/>
             </div>
