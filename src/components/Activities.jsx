b04f903f68d64ab0ff1648ad1dc881610cd6a8cd
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-21 15:43:11 +0100

    Refactor activities titles for diversity

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -13,11 +13,11 @@ const Activities = () => {
         <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={resort1} alt="/"/>
       </div>
       <div className="relative p-4 ">
-        <h3 className="z-10 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Resorts</h3>
+        <h3 className="z-10 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Cruises</h3>
         <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={resort2} alt="/" />
       </div>
       <div className="relative p-4 ">
-        <h3 className="z-10 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Resorts</h3>
+        <h3 className="z-10 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Excursions</h3>
         <img className="w-full h-full object-cover relative border-4 border-white shadow-lg" src={resort3} alt="/" />
       </div>
     </div>
