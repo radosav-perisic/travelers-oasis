25ed90d2ab5c643f4143fb4dcdd1c038407b01e8
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-13 12:22:23 +0200

    Adjust intro text div positioning

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         className="w-full h-full object-cover"
       />
       <div className="max-w-[1140px] m-auto">
-        <div className="absolute top-[40%] w-full md:-[50%] max-w-[600px] flex flex-col text-white p-4">
+        <div className="absolute top-[30%] w-full lg:top-[40%] md:top-[50%] max-w-[600px] flex flex-col text-white p-4">
           <h1 className="font-bold text-4xl">Find Your Special Trip</h1>
           <h2 className="text-4xl py-4 italic">With Traveler's Oasis</h2>
           <p>
